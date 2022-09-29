0458cb80574de04ad2e73ede1e1ae96b4e3fa1fe
Singh Gaurav Ashokkumar <110033104+gaurav62472744@users.noreply.github.com>
2022-09-29 14:50:42 +0530

    modified

---
+++ components/footer.js
@@ -0,0 +1,88 @@
+const footer = () => {
+  return ` <div id="bottom_first_row">
+    <div class="footer_box">
+        <ul>
+            <h2>OUR POLICIES</h2>
+            <li><a href="">Covid-19 Precautions</a></li>
+            <li><a href="">Our Promise</a></li>
+            <li><a href="">Terms & Conditions</a></li>
+            <li><a href="">Privacy Policy</a></li>
+        </ul>
+    </div>
+    <div class="footer_box">
+        <ul>
+            <h2>CORPORATE</h2>
+            <li><a href="">Sell With Us</a></li>
+            <li><a href="">Career Opportunities</a></li>
+            <li><a href="">Sitemap</a></li>
+        </ul>
+    </div>
+    <div class="footer_box">
+        <ul>
+            <h2>HELP</h2>
+            <li><a href="">How MeanBuy Works</a>
+            <li><a href="">My Orders</a></li>
+            <li><a href="">Exchanges, Returns and Refunds</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">FAQ</a></li>
+        </ul>
+    </div>
+    <div class="footer_box">
+        <ul>
+            <h2 class="heading">SELL WITH US</h2>
+            <li><a href="">General Information</a></li>
+            <li><a href="">Signup</a></li>
+        </ul>
+    </div>
+    <div class="footer_box">
+        <div id="email_section">
+            <input type="email" placeholder="Enter Email Address">
+            <button> Subscribe</button>
+        </div>
+        <div id="social_logo">
+            <img class="footer_logo" src="https://cdn-icons-png.flaticon.com/128/1409/1409946.png" alt="">
+            <img class="footer_logo" src="https://cdn-icons-png.flaticon.com/128/1384/1384060.png" alt="">
+            <img class="footer_logo" src="https://cdn-icons-png.flaticon.com/128/3536/3536505.png" alt="">
+            <img class="footer_logo" src="https://cdn-icons-png.flaticon.com/128/6033/6033716.png" alt="">
+        </div>
+        <div id="payment_logo">
+            <img src="https://i.ibb.co/TmTWQkg/2285.jpg" alt="">
+        </div>
+        <div id="meanbuy_offer">
+            <img src="https://i.ibb.co/NLhXvfG/meanbuy.png" alt="">
+        </div>
+    </div>
+</div>
+<div id="bottom_second_row">
+    <h3 class="orange">Meanbuy Is Your One Stop Destination For Online Shopping</h3>
+    <p>Meanbuy is a comprehensive platform for you to choose how you shop, whether it is buying instantly,
+        waiting to save or to pick up your order from the store after freezing it.</p>
+    <p>Everyone will agree that it is not easy to go down to the market in massive traffic, driving through
+        crowds, all striving for the same trendy clothing, expecting in line to try on the picked up items in
+        the trial rooms, and finally deciding on the product you buy. Some people like to personally analyze the
+        product before purchasing it in the store. Although some people do not like to shop online, others beg
+        to differ by using the internet to do things. For all you internet enthusiasts who like online shopping,
+    </p>
+    <h3 class="orange"> Get The Best Of Your Online Shopping Experience</h3>
+    <p>Keeping mind the fact that everything is just a tap away in today's world, fashion and clothing have no
+        escape from the cyberspace. Online shopping is made easier with various categories and filters that let
+        you get the best result of what you're looking for. At Meanbuy.com we specialize in categorizing the
+        required fields as per your convenience to provide you the best online shopping experience. Categories
+        like Online shopping for kids, online shopping for men, online shopping for women, online shopping for
+        accessories, etc. is curated to make this experience even more convenient. Find the Best Online Shopping
+        in Hyderabad only at Meanbuy.</p>
+
+    <h3 class="orange">Meanbuy Let You choose At Your Convenience</h3>
+    <p>What if you belong to the former kind, where you like to buy the product from the shop after analyzing it
+        personally? But don't want to miss out on a fantastic product in the process? We got your back! At
+        Meanbuy.com, you can freeze the product and pick up from your neighboring store without the worry of
+        missing the product. Another additional feature of this website is, you can wait and save on the
+        products you like.</p>
+</div>
+
+<div id="bottom_end">
+    <h3> Prodigious Digital Solution © 2022</h3>
+</div>`;
+};
+
+export { footer };
---
+++ components/navbar.js
@@ -0,0 +1,160 @@
+const navbar = () => {
+  return `<div id="row1">
+    <p>Up to Rs.500 Off on Prepaid Purchases - Use Coupon </p>
+    <p>"PREPAID"</p>
+    <img class="navbarlogo" src="https://cdn-icons-png.flaticon.com/128/1409/1409946.png" alt="">
+    <img class="navbarlogo" src="https://cdn-icons-png.flaticon.com/128/3536/3536505.png" alt="">
+    <img class="navbarlogo" src="https://cdn-icons-png.flaticon.com/128/6033/6033716.png" alt="">
+    <img class="navbarlogo" src="https://cdn-icons-png.flaticon.com/512/2736/2736014.png" alt="">
+    <p>Newsletter</p>
+</div>
+<div id="row2">
+    <div id="row2left">
+        <a href="index.html"><img id="imgLogo"
+                src="https://meanbuyassets.s3.ap-south-1.amazonaws.com/baselogo.png" alt=""></a>
+        <div id="searchbox">
+            <form class="searchB" action="">
+                <input id="inputsearch" type="search" placeholder="Search" />
+                <!-- <div class="searchImg"><img id="glass" src="search.png" alt=""></div> -->
+                <img id="searchlogo" src="https://d64lkarmo2mrq.cloudfront.net/icons/search.png" alt="">
+            </form>
+        </div>
+    </div>
+    <div id="row2mid">
+        <div id="links">
+            <p>Seller's Corner | </p>
+            <p>Feedback | </p>
+            <p>Delivery Info</p>
+        </div>
+        <div id="log">
+            <div id="smalllogin">
+                <!-- <i class="fa fa-user-o" aria-hidden="true"></i> -->
+                <img id="loginLogo" src="https://cdn-icons-png.flaticon.com/128/2321/2321232.png" alt="">
+                <a href="login.html" class="navlogin">LOGIN <span> / </span></a><a class="navlogin"
+                    href="signup.html"> SIGN UP</a>
+            </div>
+            <p id="log2">WELCOME GUEST</p>
+        </div>
+        <div id="cart">
+            <div id="carIm">
+                <img class="cartimg" src="http://www.clker.com/cliparts/O/N/s/Q/P/Y/shopping-cart-orange-th.png"
+                    alt="">
+                <p id="cartcount">0</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="row3">
+    <li id="categories1"><a href="">Categories</a>
+        <div>
+            <div id="dropdown_list">
+                <ul>
+                    <li><a href="">
+                            <h4>MEN</h4>
+                        </a></li>
+
+                    <li><a href="">Watches</a></li>
+                    <li><a href="">
+                            <h3>WOMEN</h3>
+                        </a></li>
+                    <li><a href="">Makeup Accessories</a></li>
+                    <li><a href="">clothing</a></li>
+                    <li><a href="">Handbags & Cluthes</a></li>
+                    <li><a href="">Watches</a></li>
+                    <li><a href="">Ballet Flats</a></li>
+                    <li><a href="">Sandals</a></li>
+                </ul>
+
+                <ul>
+                    <li><a href="">
+                            <h4>ELECTRONICS</h4>
+                        </a></li>
+
+                    <li><a href="">Gaming Accessories</a></li>
+                    <li><a href="">Keyboards & Mouse</a></li>
+                    <li><a href="">Small Appliances</a></li>
+
+                </ul>
+
+                <ul>
+                    <li><a href="">
+                            <h4>ESSENTIALS</h4>
+                        </a></li>
+                    <li><a href="">Home Essentials</a></li>
+                    <li><a href="">Mens's Essentials</a></li>
+                    <li><a href="">Women Essentials</a></li>
+                </ul>
+
+                <ul>
+                    <li><a href="">
+                            <h4>FLASH SALE</h4>
+                        </a></li>
+
+                    <li><a href="">Flash Sale</a></li>
+                </ul>
+
+                <ul>
+                    <li><a href="">
+                            <h4>HOME & LIVING</h4>
+                        </a></li>
+                    <li><a href="">Bath & Storage</a></li>
+                    <li><a href="">Fitness Merchandise</a></li>
+                    <li><a href="">Gardening</a></li>
+                    <li><a href="">Home Decor</a></li>
+                    <li><a href="">Home Improvement</a></li>
+                    <li><a href="">Kitchen Wares</a></li>
+                    <li><a href="">Nordic Ceiling Lights</a></li>
+                    <li><a href="">Pet Products</a></li>
+                </ul>
+
+                <ul>
+                    <li><a href="">
+                            <h4>KIDS & MOM</h4>
+                        </a></li>
+
+                    <li><a href="">Action Figures</a></li>
+                    <li><a href="">Discovery Adventure</a></li>
+                    <li><a href="">Disney</a></li>
+                    <li><a href="">Hello Kitty</a></li>
+                    <li><a href="">Kid's Shoes</a></li>
+                    <li><a href="">Marvel</a></li>
+                    <li><a href="">Minions</a></li>
+                    <li><a href="">School Backpacks</a></li>
+                    <li><a href="">Toys</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">
+                            <h4>MEANBUY DEALS</h4>
+                        </a></li>
+
+                    <li><a href="">Gifts</a></li>
+                    <li><a href="">Electronics</a></li>
+                    <li><a href="">Men's Watches</a></li>
+                    <li><a href="">Top Selling Smart Watches</a></li>
+
+                </ul>
+                <ul>
+                    <li><a href="">
+                            <h4>MOBILES & TABS</h4>
+                        </a></li>
+
+                    <li><a href="">Bluethooth Earphone</a></li>
+                    <li><a href="">Mobile Accessories</a></li>
+                    <li><a href="">Sporting Bluetooth</a></li>
+                    <li><a href="">Wired Headphones</a></li>
+                    <li><a href="">Wireless Chargers</a></li>
+                    <li><a href="">Wireless Speakers</a></li>
+                </ul>
+            </div>
+        </div>
+    </li>
+
+    <li><a href="">Flash Sale</a></li>
+    <li><a href="">Shop by brand</a></li>
+    <li><a href="">Trending</a></li>
+    <li><a href="">New Arrival</a></li>
+
+</div>`;
+};
+
+export { navbar };
